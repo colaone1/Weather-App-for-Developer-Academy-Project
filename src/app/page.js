bc5c77ed89b903ce4a0d2fa66a46f7ed2f98bfa9
EKomin220 <ifa.komin@gmail.com>
2025-05-14 09:24:17 +0100

    add function to fetch weather for a single city

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
-
+"use client";
+import ApiClient from "../../ApiClient/client";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+
   
   return (
     <div>
-
+     
     </div>
              
   );
