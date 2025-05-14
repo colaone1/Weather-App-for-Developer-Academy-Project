8b81b175351bcc4534ff52bd5128fcfddcc3a6ee
SaroltaBartos <sbartos03@gmail.com>
2025-05-14 10:43:54 +0100

    Adding some changes to weather card and page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,30 @@
-"use client"; 
-import { useState, useEffect } from "react"; 
-import ApiClient from "../../ApiClient/client"; 
+"use client";
+import { useState, useEffect } from "react";
+import ApiClient from "../ApiClient/client";
 import WeatherCard from "../components/WeatherCard";
-
 export default function Home() {
   const [days, setDays] = useState([]);
   const [loading, setLoading] = useState(false);
   const [selectedCity, setCity] = useState("");
   const [error, setError] = useState(null);
-
   const client = new ApiClient();
-
   const fetchWeather = async () => {
     try {
       if (!selectedCity) return;
-      const [latitude, longitude] = selectedCity.split(","); 
-      const response = await client.getWeatherByFilters({ latitude, longitude }); 
+      setLoading(true);
+      setError(null);
+      const [latitude, longitude] = selectedCity.split(",");
+      const response = await client.getWeatherByFilters({ latitude, longitude });
       setDays(response.data);
-      setLoading(false);
     } catch (error) {
       setError("Something went wrong. Mistakes happen.");
+    } finally {
+      setLoading(false);
     }
   };
-
   useEffect(() => {
     fetchWeather();
   }, [selectedCity]);
-
   const formattedDays = days.time?.map((date, index) => ({
     day: date,
     maxTemp: days.temperature_2m_max[index],
@@ -36,7 +34,6 @@ export default function Home() {
     precipChance: days.precipitation_probability_max[index],
     weatherCode: days.weather_code[index]
   }));
-
   return (
     <main className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 p-12">
       {error && <div className="text-red-500">{error}</div>}
@@ -49,36 +46,36 @@ export default function Home() {
         </div>
       </div>
       <select
-       value={selectedCity}
-       onChange={(e) => setCity(e.target.value)}
-       className="mb-6 p-2 border rounded"
-     >
-       <option value="">All</option>
-       <option value="51.5085,-0.1257">London</option>
-       <option value="40.4165,-3.7026">Madrid</option>
-       <option value="35.6895,139.6917">Tokyo</option>
-       <option value="40.7143,-74.006">New York</option>
-       <option value="-22.9064,-43.1822">Rio de Janeiro</option>
-       <option value="-33.8678,151.2073">Sydney</option>
-     </select>   
-
-     {loading ? (
-  <div className="text-center text-xl font-semibold">Loading...</div>
-  ) : (
-  <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-    {formattedDays?.map((day, index) => (
-      <WeatherCard
-        key={index}
-        day={day.day}
-        maxTemp={day.maxTemp}
-        minTemp={day.minTemp}
-        windSpeed={day.windSpeed}
-        precipSum={day.precipSum}
-        precipChance={day.precipChance}
-        weatherCode={day.weatherCode}
-      />
-    ))}
-    </div>
-  )}
-</main>
-)}; 
+        value={selectedCity}
+        onChange={(e) => setCity(e.target.value)}
+        className="mb-6 p-2 border rounded"
+      >
+        <option value="">Select a city</option>
+        <option value="51.5085,-0.1257">London</option>
+        <option value="40.4165,-3.7026">Madrid</option>
+        <option value="35.6895,139.6917">Tokyo</option>
+        <option value="40.7143,-74.006">New York</option>
+        <option value="-22.9064,-43.1822">Rio de Janeiro</option>
+        <option value="-33.8678,151.2073">Sydney</option>
+      </select>
+      {loading ? (
+        <div className="text-center text-xl font-semibold">Loading...</div>
+      ) : (
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {formattedDays?.map((day, index) => (
+            <WeatherCard
+              key={index}
+              day={day.day}
+              maxTemp={day.maxTemp}
+              minTemp={day.minTemp}
+              windSpeed={day.windSpeed}
+              precipSum={day.precipSum}
+              precipChance={day.precipChance}
+              weatherCode={day.weatherCode}
+            />
+          ))}
+        </div>
+      )}
+    </main>
+  );
+}
